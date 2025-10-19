06cbf247f850c8d7d95dcb6736be853f741e6e3c
nithish847 <nithish.gujjala07@gmail.com>
2025-10-20 00:36:19 +0530

    ready to deploy project ,.

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,11 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: ["http://localhost:5173", "https://tripweave-1-f.onrender.com"], 
+  // frontend URL
   credentials: true,
 }));
+
 
 // Routes
 app.use("/api/places", placeRoutes);
